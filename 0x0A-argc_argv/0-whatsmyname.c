f0ceab967b3cf3373ab9bc485bb7b01787d249af
Walidbn0 <waliidbenhammii@gmail.com>
2023-10-02 03:22:31 -0700

    argc argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
-#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * main- a program that prints its name
@@ -9,23 +10,10 @@
  * Return: Always 0 (Success)
 */
 
-void ft_putstr(char *str)
+int main(int __attribute__((unused)) argc, char *argv[])
 {
-    int i;
-    i = 0;
-    while(str[i] != '\0')
-    {
-         write(1, &str[i], 1);
-        i++;
-    }
+	int i = 0;
 
-}
-
-int main(int argc, char **argv)
-{
-    int j;
-    j = argc;
-    ft_putstr(argv[0]);
-    write(1, "\n", 1);
-    return 0;
+	printf("%s\n", argv[i]);
+	return (0);
 }
